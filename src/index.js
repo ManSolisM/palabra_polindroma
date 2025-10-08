111bc0a4f2232955b323c862949e05d157684be8
ManSolisM <trabajscolar@gmail.com>
2025-10-08 11:16:57 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../public/css/main.css';
-import Swal from "sweetalert2";
 import Polindromo from './polindromo.js';
-
 
 // Inicializar el verificador de palíndromos
 const verificador = new Polindromo();
